d291257d21af088089eaa7a37008d764fa4ed1bb
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-05-22 00:47:28 -0400

    Updated executable

--- bin/scrabble.rb
+++ bin/scrabble.rb
@@ -4,16 +4,23 @@ require_relative "../lib/tile.rb"
 
 Tile.include Scoring
 
-puts "\n#{"Welcome to Scrabble!".center(70)}\n\n\n"
+puts "\n#{"Welcome to Scrabble!".center(50)}\n\n\n"
 
 puts "Would you like to play single player(1) or multiplayer(2)?"
-play_mode = get_input
-play_mode = validate_play_mode
+
+play_mode_answers = ['1', '2']
+
+puts "Choices are #{play_mode_answers}"
+play_mode = gets.chomp
 
 puts
 
 puts "Player one"
 print "Please enter your word: "
-word = get_input
-word = validate_word
-puts "Your score was: #{Tile.new(word).score}\n\n"
+word = gets.chomp
+
+begin
+  puts "Your score was: #{Tile.new(word).score}\n\n"
+rescue StandardError
+  puts "Your word must only contain valid tiles"
+end
