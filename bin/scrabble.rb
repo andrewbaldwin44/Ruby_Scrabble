3c8c02a5ab0451d9d23dac48fff2e2721405bbd6
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-05-21 23:09:21 -0400

    Deleted style for now

--- bin/scrabble.rb
+++ bin/scrabble.rb
@@ -1,14 +1,11 @@
 require_relative '../lib/board'
 require_relative '../lib/scoring'
-require_relative "../lib/style.rb"
 require_relative "../lib/tile.rb"
 
 Tile.include Scoring
 Tile.include Board
 
-include TextStyles
-
-puts "\n#{"Welcome to Scrabble!".red_white_highlight.center(70)}\n\n\n"
+puts "\n#{"Welcome to Scrabble!".center(70)}\n\n\n"
 
 puts "Would you like to play single player(1) or multiplayer(2)?"
 play_mode = get_input
@@ -16,7 +13,7 @@ play_mode = validate_play_mode
 
 puts
 
-puts "Player one".blue_block.underline
+puts "Player one"
 print "Please enter your word: "
 word = get_input
 word = validate_word
