0151af5d1f8aeb5552dbf9c7ac0b5ab3cf0607c4
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-05-31 00:43:35 -0400

    Display board with matrix

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,18 @@ require_relative '../lib/scoring'
 
 class Board < Tile
   def initialize
+    @board = [
+      ['| 3w ', '    ', '    ', ' 2l ', '    ', '    ', '    ', ' 3w ', '    ', '    ', '    ', ' 2l ', '    ', '    ', ' 3w |'],
+      ['|    ', ' 2w ', '    ', '    ', '    ', ' 3l ', '    ', '    ', '    ', ' 3l ', '    ', '    ', '    ', ' 2w ', '    |'],
+      ['|    ', '    ', ' 2w ', '    ', '    ', '    ', ' 2l ', '    ', ' 2l ', '    ', '    ', '    ', ' 2w ', '    ', '    |'],
+      ['| 2l ', '    ', '    ', ' 2w ', '    ', '    ', '    ', ' 2l ', '    ', '    ', '    ', ' 2w ', '    ', '    ', ' 2l |'],
+      ['|    ', '    ', '    ', '    ', ' 2w ', '    ', '    ', '    ', '    ', '    ', ' 2w ', '    ', '    ', '    ', '    |'],
+      ['|    ', ' 3l ', '    ', '    ', '    ', ' 3l ', '    ', '    ', '    ', ' 3l ', '    ', '    ', '    ', ' 3l ', '    |'],
+      ['|    ', '    ', ' 2l ', '    ', '    ', '    ', ' 2l ', '    ', ' 2l ', '    ', '    ', '    ', ' 2l ', '    ', '    |'],
+      ['| 3w ', '    ', '    ', ' 2l ', '    ', '    ', '    ', ' ** ', '    ', '    ', '    ', ' 2l ', '    ', '    ', ' 3w |'],
+    ]
+
+    (0...@board.length - 1).reverse_each { |index| @board << @board[index] }
   end
 
   include Scoring
@@ -13,7 +25,21 @@ class Board < Tile
 
   public
 
+  attr_accessor :board
   attr_reader :values
+
+  def display
+    first_row = '|    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |'
+    row_seperator = '|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|'
+
+    board.each_with_index.each_with_object(matrix = "") do |row, index|
+      matrix << first_row + "\n" if index == 0
+      matrix << row_seperator + "\n"
+      matrix << row.join('|') + "\n"
+      matrix << row_seperator + "\n" if index == board.length - 1
+      matrix
+    end
+  end
 
   def new_letters(letters)
     @game_tiles = Tile.new(letters)
