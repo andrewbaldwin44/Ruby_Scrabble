df48b980eb009cd68f7109a97b9179f501312c18
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-21 11:48:49 -0400

    Board rjust in a one place
    Message-Id: <20200621154849.12369-3-andrew.baldwin44@gmail.com>

--- lib/board.rb
+++ lib/board.rb
@@ -80,8 +80,9 @@ class Board < Tile
     end
 
     row_seperator = (['|'] * Geometry[:size].next).join('-' * Geometry[:field_width])
+    offset_row = "%s\n".rjust(Geometry[:board_offset])
 
-    matrix = "%s\n".rjust(Geometry[:board_offset]) % row_markers
+    matrix = offset_row % row_markers
 
     board_display = board.map { |row| row.map do |square|
       square.to_s.center(Geometry[:field_width])
