3eb33884a2a0fc66bae95f075d4faf92cbd03b95
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-21 11:46:14 -0400

    Rename test board constant
    Message-Id: <20200621154614.12172-3-andrew.baldwin44@gmail.com>

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,52 +3,52 @@ require 'minitest/pride'
 require_relative '../lib/board'
 
 describe Board do
-  BOARD = Board.new
+  Test_Board = Board.new
 
   it 'must score letters' do
-    expect(BOARD.new_letters('orchard').score).must_equal 13
+    expect(Test_Board.new_letters('orchard').score).must_equal 13
   end
 
   it 'must double letters' do
-    actual = BOARD.new_letters('mellifluous').multiply('lf', 2).score
+    actual = Test_Board.new_letters('mellifluous').multiply('lf', 2).score
     expect(actual).must_equal 21
   end
 
   it 'must triple letters' do
-    actual = BOARD.new_letters('somnambulist').multiply('b', 3).score
+    actual = Test_Board.new_letters('somnambulist').multiply('b', 3).score
     expect(actual).must_equal 24
   end
 
   it 'must double words' do
-    actual = BOARD.new_letters('effervescence').word_times(2).score
+    actual = Test_Board.new_letters('effervescence').word_times(2).score
     expect(actual).must_equal 52
   end
 
   it 'must triple words' do
-    actual = BOARD.new_letters('ineffable').word_times(3).score
+    actual = Test_Board.new_letters('ineffable').word_times(3).score
     expect(actual).must_equal 51
   end
 
   it 'must double and triple letters' do
-    actual = BOARD.new_letters('petrichor').multiply('p', 2)
+    actual = Test_Board.new_letters('petrichor').multiply('p', 2)
                   .multiply('h', 3).score
     expect(actual).must_equal 27
   end
 
   it 'must double and triple word' do
-    actual = BOARD.new_letters('defenestration').word_times(2)
+    actual = Test_Board.new_letters('defenestration').word_times(2)
                   .word_times(3).score
     expect(actual).must_equal 108
   end
 
   it 'must double and triple word and double and triple letters' do
-    actual = BOARD.new_letters('oxazepam').multiply('xp', 2)
+    actual = Test_Board.new_letters('oxazepam').multiply('xp', 2)
                   .multiply('z', 3).word_times(2).word_times(3).score
     expect(actual).must_equal 354
   end
 
   it 'must multiply a word with blanks' do
-    actual = BOARD.new_letters('ram unctious').multiply('m', 2)
+    actual = Test_Board.new_letters('ram unctious').multiply('m', 2)
                   .word_times(2).word_times(3).score
     expect(actual).must_equal 108
   end
